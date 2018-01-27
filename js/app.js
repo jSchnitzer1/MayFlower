9902f533162090f125b28f89187374e5c7eea505
khaled <khaledj@kth.se>
2018-01-27 01:23:17 +0100

    Implementing mvc for the project + implement one page with multiple screens

--- js/app.js
+++ js/app.js
@@ -1,38 +1,20 @@
-$(function() {
-	//We instantiate our model
-	var model = new DinnerModel();
-	
-	// And create the instance of ExampleView
-	var dinnerView = new DinnerView($('.dinner_view'));
+/*
+ * Coded by:
+ * - Khaled Jendi
+ * - Camilla Björn
+ * - Last update in 2018-01-26
+ */
 
-	$('#vide_container').vide({
+$(function() {
+    // view, model, controller are defined as global variables (see index.html)
+
+    // load main video background (this is general everywhere, loaded once and for all)
+    $('#general_view').vide({
         mp4: "video/rst1.mp4",
         poster: "detect"
     });
 
-    $('.heading').addClass('hanimate');
-    $('.subheading').addClass('sanimate');
-    $('.reserve').addClass('ranimate');
-
-    $('.nav-menu-toggle, .nav-menu .close').on('click', function() {
-        $('.nav-menu-wrap').toggleClass('active');
-    });
-
-    $(window).resize(function() {
-        if($(window).width() > 990) {
-            if ($(".nav-menu-wrap").hasClass("active")) {
-                $('.nav-menu-wrap').toggleClass('active');
-            }
-        }
-    });
-
-
-
-	/**
-	 * IMPORTANT: app.js is the only place where you are allowed to
-	 * use the $('someSelector') to search for elements in the whole HTML.
-	 * In other places you should limit the search only to the children 
-	 * of the specific view you're working with (see exampleView.js).
-	 */
+    // load dinner model: loaded once and for all!
+    model = new DinnerModel();
 
 });
